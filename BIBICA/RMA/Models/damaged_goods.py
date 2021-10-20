4d116cd81ccc9852c45f9fffecee5c4b285d8ff5
phantenphuc12 <baophuc12k@gmail.com>
2021-10-21 00:11:33 +0700

    Create damaged_goods.py

---
+++ BIBICA/RMA/Models/damaged_goods.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models, tools
+
+
+class Damaged_goods(models.Model):
+    _name = "damaged.goods"
+
+    name = fields.Many2one('product.template', 'name', String="Tên Sản Phẩm")
+    processing_date = fields.Date(string="Ngày Xử Lý Hàng Hóa")
+    quantity = fields.Integer(string="Số Lượng")
+    input_date = fields.Date(string="Ngày Nhập Kho Sản Phẩm")
+    description = fields.Text(string="Mô Tả")
+    status = fields.Selection([
+        ("tieuhuy", "Tiêu Hủy"),
+        ("xahang", "Xả Hàng"),
+    ], string="Trạng Thái", default=False)
