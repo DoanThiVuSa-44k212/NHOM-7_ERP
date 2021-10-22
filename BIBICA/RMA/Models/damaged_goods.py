9a51d9dfdcb8548a56596196f3c832d2546d1d1b
phantenphuc12 <baophuc12k@gmail.com>
2021-10-22 23:22:45 +0700

    Update damaged_goods.py

--- BIBICA/RMA/Models/damaged_goods.py
+++ BIBICA/RMA/Models/damaged_goods.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, tools
 class Damaged_goods(models.Model):
     _name = "damaged.goods"
 
-    name = fields.Many2many('product.template', 'name', String="Tên Sản Phẩm")
+    name = fields.Many2many('product.template', String="Tên Sản Phẩm")
     processing_date = fields.Date(string="Ngày Xử Lý Hàng Hóa")
     quantity = fields.Integer(string="Số Lượng")
     input_date = fields.Date(string="Ngày Nhập Kho Sản Phẩm")
