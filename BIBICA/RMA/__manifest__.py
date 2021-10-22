2d22713fac0937413391e5c0d4f8ff7eba30b51f
phantenphuc12 <baophuc12k@gmail.com>
2021-10-22 17:55:31 +0700

    Create __manifest__.py

---
+++ BIBICA/RMA/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'RMA BIBICA',
+    'version': '1.0',
+    'summary': 'Return Merchandise Authorization',
+    'sequence': -10,
+    'description': """RMA BIBICA""",
+    'category': 'Productivity',
+    'license': 'LGPL-3',
+    'depends': [
+        'product'
+    ],
+    'data': [
+        'Views/Products.xml',
+        'Views/damaged_goods.xml',
+        'Views/productpreserve.xml',
+        'Menu/Menu.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
