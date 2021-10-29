77bcf61592774efaf6469a9b9dc2da37c4e2f598
Nguyen-Truong-Sinh <sinhtruong311@gmail.com>
2021-10-30 00:13:00 +0700

    Update productpreserve.py

--- BIBICA/RMA/Models/productpreserve.py
+++ BIBICA/RMA/Models/productpreserve.py
@@ -6,9 +6,17 @@ class Inventory(models.Model):
     _description = "RMA Product Preserve"
 
     # datebq=fields.Date(string='Ngày nhập kho bảo quản')
+    Nhietdo=fields.Selection([
+        ('1', '10c->20c'),
+        ('2', '20c->30c'),
+    ],string="Nhiệt độ bảo quản", required=True, default='1')
+    datebq=fields.Date(string='Ngày nhập kho bảo quản')
+    datehh=fields.Date(string='Ngày nhập kho hàng hóa')
+    thoigianbaoquan = fields.Date(string='Thời gian bảo quản')
     Kho = fields.Selection([
         ('1', 'Kho 1'),
         ('2', 'Kho 2'),
         ('3', 'Kho 3'),
-    ], required=True, default='1')
+    ],string="Nơi bảo quản", required=True, default='1')
+
 
