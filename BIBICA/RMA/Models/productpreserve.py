0319cb3bce7d517c6ecad1392c11cceddd73d0aa
phantenphuc12 <baophuc12k@gmail.com>
2021-10-29 20:25:54 +0700

    Update productpreserve.py

--- BIBICA/RMA/Models/productpreserve.py
+++ BIBICA/RMA/Models/productpreserve.py
@@ -1,14 +1,11 @@
 from odoo import api, fields, models
 
-class rmaproductpreserve(models.Model):
-    _name = "rma.productpreserve"
+class Inventory(models.Model):
+    _name = "stock.inventory"
+    _inherit = "stock.inventory"
     _description = "RMA Product Preserve"
 
-    pname= fields.Many2many('product.template', string="Tên SP")
-    Ma_Hang = fields.Integer(string='Mã hàng')
-    datebq=fields.Date(string='Ngày nhập kho bảo quản')
-    datehh = fields.Date(string='Ngày nhập kho hàng hóa')
-    So_luong = fields.Integer(string='Số lượng')
+    # datebq=fields.Date(string='Ngày nhập kho bảo quản')
     Kho = fields.Selection([
         ('1', 'Kho 1'),
         ('2', 'Kho 2'),
