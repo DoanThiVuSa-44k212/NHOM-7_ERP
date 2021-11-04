fe090bec423051e31b92380ab5ae406086c6cecb
phantenphuc12 <baophuc12k@gmail.com>
2021-11-04 16:13:52 +0700

    Update Products.py

--- BIBICA/RMA/Models/Products.py
+++ BIBICA/RMA/Models/Products.py
@@ -10,3 +10,15 @@ class ProductTemplate(models.Model):
     weight = fields.Integer(string='Cân Nặng')
     pr_type = fields.Many2one(comodel_name='product.type', string="Loại Sản Phẩm", delegate=True)
     unit_of_measure = fields.Many2one('uom.uom', related="pr_type.unit_of_measure", string='Đơn Vị Tính')
+    # hansudung = fields.Char(string="Hạn Sử Dụng")
+    # baohanh = fields.Char(string="Bảo Hành")
+    thanhphan_lines = fields.One2many('product.template.lines', 'thanhphan_id')
+
+class ProductTemplateLines(models.Model):
+    _name = 'product.template.lines'
+
+    tentp = fields.Many2one('product.ingredients', string="Tên Thành Phần")
+    soluong = fields.Integer(string="Tỷ Lệ %")
+    congdung = fields.Text(related="tentp.congdung", string="Công Dụng")
+    thanhphan_id = fields.Many2one('product.template')
+
