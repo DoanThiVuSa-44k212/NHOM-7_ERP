12c7a1fb7a8285ee281a5a0e0872b655b1973594
Nguyen-Truong-Sinh <78039319+Nguyen-Truong-Sinh@users.noreply.github.com>
2021-11-10 23:29:40 +0700

    Update productpreserve.py

--- BIBICA/RMA/Models/productpreserve.py
+++ BIBICA/RMA/Models/productpreserve.py
@@ -3,7 +3,6 @@ from odoo import api, fields, models
 
 class productpreverse(models.Model):
     _name = "product.preserve"
-
     _description = "RMA Product Preserve"
 
     name = fields.Char(String="Tên Nghiệp Vụ")
@@ -17,19 +16,26 @@ class productpreverse(models.Model):
     datebq = fields.Date(string='Ngày nhập kho bảo quản')
     datehh = fields.Date(string='Ngày nhập kho hàng hóa')
     thoigianbaoquan = fields.Date(string='Thời gian bảo quản')
+    datechamsocdinhki = fields.Date(string='Ngày theo dõi hàng hóa định kì')
     Kho = fields.Many2one('temporary.warehouse', String="Nơi Bảo Quản")
     diachi = fields.Text(related="Kho.diachi", string="Địa chỉ")
-    dientich = fields.Integer(related="Kho.dientich", string="Diện Tích (Ha)")
+    dientich = fields.Char(related="Kho.dientich", string="Diện Tích (Ha)")
+    tinhtranghang = fields.Selection([
+        ('1', 'Dưới 40%'),
+        ('2', 'Trên 40%'),
+    ], string="Tình trạng hàng", required=True, default='1')
 
 
 class productpreverselines(models.Model):
 
     _name = "product.preserve.lines"
+    # nhanbiet = fields.Char(string="nhanbiet")
     product_id = fields.Many2one('product.template', string="Tên Sản Phẩm")
     product_qty = fields.Integer(string="Số Lượng")
     list_price = fields.Float(string='Giá Bán', related="product_id.list_price")
     sum_price = fields.Integer(string="Tổng Tiền", compute="_compute_sum_price")
     preserve_id = fields.Many2one('product.preserve', string="Product Preserve")
+    # test = fields.Integer(string="Số Lượng Hiện tại", compute="_compute_test")
 
 
     def _compute_sum_price(self):
@@ -39,4 +45,3 @@ class productpreverselines(models.Model):
              sum_price += (productpreverselines.product_id.list_price
                            * productpreverselines.product_qty)
           productpreverselines.sum_price = sum_price
-
