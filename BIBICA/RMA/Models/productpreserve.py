f871216bc76e1634d4c87456c889b82fcfd1a03a
phantenphuc12 <baophuc12k@gmail.com>
2021-11-10 22:06:38 +0700

    Update productpreserve.py

--- BIBICA/RMA/Models/productpreserve.py
+++ BIBICA/RMA/Models/productpreserve.py
@@ -19,19 +19,17 @@ class productpreverse(models.Model):
     thoigianbaoquan = fields.Date(string='Thời gian bảo quản')
     Kho = fields.Many2one('temporary.warehouse', String="Nơi Bảo Quản")
     diachi = fields.Text(related="Kho.diachi", string="Địa chỉ")
-    dientich = fields.Char(related="Kho.dientich", string="Diện Tích (Ha)")
+    dientich = fields.Integer(related="Kho.dientich", string="Diện Tích (Ha)")
 
 
 class productpreverselines(models.Model):
 
     _name = "product.preserve.lines"
-    # nhanbiet = fields.Char(string="nhanbiet")
     product_id = fields.Many2one('product.template', string="Tên Sản Phẩm")
     product_qty = fields.Integer(string="Số Lượng")
     list_price = fields.Float(string='Giá Bán', related="product_id.list_price")
     sum_price = fields.Integer(string="Tổng Tiền", compute="_compute_sum_price")
     preserve_id = fields.Many2one('product.preserve', string="Product Preserve")
-    # test = fields.Integer(string="Số Lượng Hiện tại", compute="_compute_test")
 
 
     def _compute_sum_price(self):
@@ -42,11 +40,3 @@ class productpreverselines(models.Model):
                            * productpreverselines.product_qty)
           productpreverselines.sum_price = sum_price
 
-    # @api.onchange("test")
-    # def _compute_test(self):
-    #     chang_qty = self.env['stock.picking']
-    #     for productpreverselines in self:
-    #         tested = chang_qty.product_qtyy
-    #         if productpreverselines.product_qty and chang_qty.product_qtyy:
-    #             tested += (productpreverselines.product_qty - chang_qty.product_qtyy)
-    #         productpreverselines.test = tested
